5ab461cabf518abdb082683bbf8910bbd63656c7
Jackmanek <age002@gmail.com>
2024-10-03 22:51:33 +0200

    Inicio de Tarea

---
+++ src/TareaEvaluativaUD1_01.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/*
+    1.	Flujos de caracteres: (FileReader, FileWriter) (1,5 puntos)
+    Escribe un programa en Java que lea una línea de texto desde un archivo y devuelva la misma línea,
+    pero con las palabras invertidas individualmente. Es decir, si el archivo contiene "Hola Mundo",
+    el archivo de salida debería contener "aloH odnuM".
+
+ */
+
+public class TareaEvaluativaUD1_01 {
+
+
+}
