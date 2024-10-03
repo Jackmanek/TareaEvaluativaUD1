530171f7af8c1df9b69bfd07de49596b94a10418
Jackmanek <age002@gmail.com>
2024-10-04 01:46:12 +0200

    Ejercicio 3 -Finalizado

---
+++ src/TareaEvaluativaUD1_03.java
@@ -0,0 +1,45 @@
+/*
+3.- (Flujos binarios:InputStream) (1,5 puntos)
+Realiza un programa en Java que lea la cabecera de un fichero PDF y
+verifique si realmente se trata de un archivo PDF válido.
+Para que un archivo sea un PDF válido,
+debe comenzar con la siguiente secuencia de bytes: {37, 80, 68, 70}.
+Si la cabecera no coincide con esta secuencia,
+el programa debe informar al usuario de que el archivo no es válido (1 punto)
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class TareaEvaluativaUD1_03 {
+    public static void main(String[] args) {
+
+        File inpFile = new File("C://Users//Jasio//Documents//Birthl//CV_Jasmany.pdf");
+
+        int [] pdfHeader = {37, 80, 68, 70};
+
+        try {
+            InputStream inputStream = new FileInputStream(inpFile);
+
+            int [] saveFirstHeader = new int[4];
+            for (int i = 0; i < 4; i++) {
+                saveFirstHeader[i] = inputStream.read();
+            }
+            // Lectura de los primeros bytes del PDF
+            /*
+            for(int j=0; j<saveFirstHeader.length; j++){
+                System.out.println("hola->"+saveFirstHeader[j]);
+            }
+            */
+            if(!Arrays.equals(saveFirstHeader, pdfHeader)) {
+                System.out.println("No es un fichero PDF");
+                System.exit(-1);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
